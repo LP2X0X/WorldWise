5b0934e44b6dcd8c9e4ee9f9f84b59af72f72556
long.pham <phamquanglong.work@gmail.com>
2025-11-04 20:22:22 +0700

    Add new stuff

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,20 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
+import "react-datepicker/dist/react-datepicker.css";
+
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useURLPosition } from "../hooks/useURLPosition";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
-import { useURLPosition } from "../hooks/useURLPosition";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import DatePicker from "react-datepicker";
+import { useCities } from "../hooks/useCities";
+import { useNavigate } from "react-router-dom";
 
-export function convertToEmoji(countryCode) {
+function convertToEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
     .split("")
@@ -29,9 +33,12 @@ function Form() {
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [emoji, setEmoji] = useState();
   const [geocodingError, setGeocodingError] = useState("");
+  const { createCity, isLoading } = useCities();
+  const navigate = useNavigate();
 
   useEffect(
     function () {
+      if (!lat && !lng) return;
       async function fetchCityData() {
         try {
           setIsLoadingGeocoding(true);
@@ -45,7 +52,6 @@ function Form() {
             );
           }
           const data = await res.json();
-          console.log(data);
 
           if (!data.countryCode) {
             throw new Error(
@@ -68,8 +74,29 @@ function Form() {
     [lat, lng]
   );
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+    await createCity(newCity);
+    navigate("/app/cities");
+  }
+
   if (isLoadingGeocoding) {
     return <Spinner />;
+  }
+
+  if (!lat && !lng) {
+    return <Message message="Start by clicking somewhere on the map!" />;
   }
 
   if (geocodingError) {
@@ -77,7 +104,10 @@ function Form() {
   }
 
   return (
-    <form className={styles.form}>
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ""}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -90,10 +120,16 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        {/* <input
           id="date"
           onChange={(e) => setDate(e.target.value)}
           value={date}
+        /> */}
+        <DatePicker
+          id="date"
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat="dd/MM/yyyy"
         />
       </div>
 
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,28 +1,62 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const BASE_URL = "http://localhost:8000";
 
 const CitiesContext = createContext();
 
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "fetchedCity":
+      return { ...state, isLoading: false, currentCity: action.payload };
+    case "fetchedCities":
+      return { ...state, isLoading: false, cities: action.payload };
+    case "createdCity":
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+      };
+    case "deleteCity":
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+      };
+    case "loading":
+      return { ...state, isLoading: true };
+    case "doneLoading":
+      return { ...state, isLoading: false };
+    default:
+      throw new Error("Unknown action type...");
+  }
+}
+
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(function () {
     async function fetchCities() {
       try {
-        setIsLoading(true);
+        dispatch({ type: "loading" });
         let res = await fetch(`${BASE_URL}/cities`);
         if (!res.ok) {
           throw new Error("There is something wrong when fetching the data...");
         }
         let data = await res.json();
-        setCities(data);
+        dispatch({ type: "fetchedCities", payload: data });
       } catch (err) {
         console.log(err);
       } finally {
-        setIsLoading(false);
+        dispatch({ type: "doneLoading" });
       }
     }
 
@@ -32,22 +66,73 @@ function CitiesProvider({ children }) {
 
   async function getCity(id) {
     try {
-      setIsLoading(true);
+      dispatch({ type: "loading" });
       const res = await fetch(`${BASE_URL}/cities/${id}`);
       if (!res.ok) {
         throw new Error("There is something wrong when fetching city data!");
       }
       const data = await res.json();
-      setCurrentCity(data);
+      dispatch({ type: "fetchedCity", payload: data });
     } catch (err) {
       console.log(err);
     } finally {
-      setIsLoading(false);
+      dispatch({ type: "doneLoading" });
+    }
+  }
+
+  async function createCity(newCity) {
+    try {
+      dispatch({ type: "loading" });
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (!res.ok) {
+        throw new Error(
+          "There is something wrong when sending city data to the server!"
+        );
+      }
+      // Get the new city WITH the ID
+      const data = await res.json();
+      dispatch({ type: "createdCity", payload: data });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      dispatch({ type: "doneLoading" });
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      dispatch({ type: "loading" });
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      if (!res.ok) {
+        throw new Error(
+          "There is something wrong when delete city data on the server!"
+        );
+      }
+      dispatch({ type: "deleteCity", payload: id });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      dispatch({ type: "doneLoading" });
     }
   }
 
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
+    >
       {children}
     </CitiesContext.Provider>
   );
